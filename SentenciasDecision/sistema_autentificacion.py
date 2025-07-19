b10854b2cef1cc7313494aac26fc39df71b69b63
MagicOso1412 <alexisosoazul@hotmail.com>
2025-07-19 16:17:54 -0600

    Ejercicio Sistema de autentificacion completo

--- SentenciasDecision/sistema_autentificacion.py
+++ SentenciasDecision/sistema_autentificacion.py
@@ -6,4 +6,11 @@ PASSWORD_VALIDO = '123'
 usuario = input('Ingresa tu usuario: ')
 password = input('Ingresa tu password: ')
 
-if usuario = USUARIO_VALIDO and password = PASSWORD_VALIDO:
+if usuario == USUARIO_VALIDO and password == PASSWORD_VALIDO:
+    print('Bienvenido al sistema')
+elif usuario == USUARIO_VALIDO:
+    print('Password incorrecto, favor de corregir!')
+elif password == PASSWORD_VALIDO:
+    print('Usuario incorrecto, favor de corregir!')
+else:
+    print('Usuario y Password incorrectos, favor de corregirlos!')
