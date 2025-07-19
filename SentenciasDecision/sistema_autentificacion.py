c62de93ab9b40dd5ec46c6a26c261b9c0c66c458
MagicOso142 <alexisosoazul@hotmail.com>
2025-07-19 16:03:35 -0600

    Agregue ejercicio sistemas incompleto

---
+++ SentenciasDecision/sistema_autentificacion.py
@@ -0,0 +1,9 @@
+print('SISTEMA DE AUTENTIFICACION')
+
+USUARIO_VALIDO = 'admin'
+PASSWORD_VALIDO = '123'
+
+usuario = input('Ingresa tu usuario: ')
+password = input('Ingresa tu password: ')
+
+if usuario = USUARIO_VALIDO and password = PASSWORD_VALIDO:
